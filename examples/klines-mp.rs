31f533a9473749f444b0b8f284584bee7e4a9a86
bpzj <bpzj@outlook.com>
2025-09-21 23:04:01 +0800

    多进程加速

--- examples/klines-mp.rs
+++ examples/klines-mp.rs
@@ -1,4 +1,4 @@
-use rusths::ths::{THS, Adjust, Interval};
+use rusths::ths::{THS};
 use std::process::Command;
 use std::env;
 
@@ -6,17 +6,18 @@ use std::env;
 /// 使用多个进程来加速
 fn main() {
     // 如果是子进程，执行数据获取逻辑
-    if let Ok(thread_id) = env::var("THREAD_ID") {
-        run_child_process(&thread_id);
+    if let Ok(_download) = env::var("DOWNLOAD") {
+        let thread_id = env::var("THREAD_ID").unwrap();
+        download_child_process(&thread_id);
         return;
     }
 
     // 主进程逻辑：创建多个子进程
     let mut handles = vec![];
-    
-    for i in 0..4 {
+    for i in 0..10 {
         let mut child = Command::new(env::current_exe().unwrap())
             .env("THREAD_ID", i.to_string())
+            .env("DOWNLOAD", "YES")
             .spawn()
             .expect("Failed to spawn process");
         
@@ -29,21 +30,14 @@ fn main() {
     }
 }
 
-fn run_child_process(thread_id: &str) {
-    // 在子进程中创建新的 THS 实例
-    let mut ths = THS::new(None).expect("Failed to create THS instance");
+fn download_child_process(thread_id: &str) {
+    // 在子进程中创建实例，不影响
+    let mut ths = THS::new(None).expect("Failed to create instance");
     ths.connect().expect("Failed to connect to server");
     
-    let klines = ths.klines(
-        "USHA600795",
-        Some("2024-01-01 00:00:00"),
-        Some("2025-01-01 00:00:00"),
-        Adjust::FORWARD,
-        Interval::MIN_5,
-        10
-    ).expect("Failed to get klines in process");
+    let klines = ths.tick_super_level1("USHA600795").expect("Failed to get tick in process");
 
-    println!("Process {} got klines: {:?}", thread_id, klines.payload.result);
+    println!("Process {} got klines: {:?}", thread_id, klines.payload.result.len());
 
     // 断开连接
     ths.disconnect().expect("Failed to disconnect");
